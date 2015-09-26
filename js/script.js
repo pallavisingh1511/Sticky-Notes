b9e4dc9ddc13c3b4f114ba62aba48b4948aafeac
Pallavi Singh <pallavi.singh@outlook.in>
2015-09-26 22:16:15 +0530

    Created methods and event listeners

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var db = null;
 
+//Create Databse
 if(window.openDatabase){
     db = openDatabase("NoteTest", "1.0", "Sticky Database", 10000000);
     if(!db)
@@ -8,6 +9,11 @@ if(window.openDatabase){
     alert("Failed to open database, make sure your browser supports HTML5 web storage");
 }
 
+var capture = null;
+var highestZ = 0;
+var highestIc = 0;
+
+//Note Object
 function Note(){
     var self = this;
     
@@ -53,7 +59,7 @@ function Note(){
     return this;
 }
 
-//Functions and properties to be extended by the Note object
+//Functions and properties to be extending the Note object (getters & setters)
 Note.prototype = {
     
     //Id getter & setter
@@ -120,4 +126,108 @@ Note.prototype = {
     set zIndex(x){
         this.note.style.zIndex = x;
     }, 
+    
+    close: function(e){
+        this.cancelPendingSave();
+        var note = this;
+        db.transaction(function(tx){
+            tx.executeSql("DELETE FROM MySticky WHERE id = ?", [note.id]);
+        });
+        document.body.removeChild(this.note);
+    },
+    
+    saveSoon: function(){
+        this.cancelPendingSave();
+    var self = this;
+    this._saveTimer = setTimeout(function(){
+            self.save();
+        }, 200);
+    },
+    
+    cancelPendingSave: function(){
+        if(!("_saveTimer" in this)){
+            return;
+        }
+        clearTimeout(this._saveTimer);
+        delete this._saveTimer;
+    },
+    
+    //Save an already existing sticky
+    save: function(){
+        this.cancelPendingSave();
+        if("dirty" in this){
+            this.timestamp = new Date().getTime();
+            delete this.dirty;
+        }
+        
+        var note this;
+        db.transaction(function(tx){
+            tx.executeSql("UPDATE MySticky SET note = ?, timestamp = ?, left = ?, top = ? , zindex = ? WHERE id = ?",
+                         [note.text, note.timestamp, note.left, note.top, note.zIndex, note.id]);
+        
+        });
+    },
+    
+    //Saving a new sticky
+    saveAsNew: function(){
+        this.timestamp = new Date.getTime();
+    
+        var note = this;
+        db.transaction(function(tx){
+        tx.executeSql("INSERT INTO MySticky SET (id, note, timestamp, left, top, zindex) VALUES (?, ?, ?, ?, ?)",
+                             [note.id, note.text, note.timestamp, note.left, note.top, note.zIndex]);
+
+        });
+    },
+    
+    // On the click of mouse button
+    onMouseDown: function(e){
+        capture = this;
+        this.startX = e.clientX - this.note.offsetLeft;
+        this.startY = e.clientY - this.note.offsetTop;
+        this.zIndex = ++highestZ;
+        
+        var self = this;
+        if(!("mouseMoveHandler" in this)){
+            this.mouseMoveHandler = function(e){return self.onMouseMove(e)}
+            this.mouseUpHandler = function(e){return self.onMouseUp(e)}
+        }
+        //Create evenetlisteners
+        document.addEventListener("mousemove", this.mouseMoveHandler, true);
+        document.addEventListener("mouseup", this.mouseUpHandler, true);
+    },
+
+    // On the drag of mouse  
+     onMouseMove: function(e){
+        if(this != captured){
+            return true;
+        }
+         this.left = e.clientX - this.startX + 'px';
+         this.top = e.clientY - this.startY + 'px';
+         return false;
+     },
+
+    // On the release of mouse button
+     onMouseUp: function(e){
+
+         document.removeEventListener("mousemove", this.mouseOverHandler, true);
+         document.removeEventListener("mouseup", this.mouseUpHandler, true);
+
+         this.save();
+         return false;
+     },
+
+    //On click of node
+    onNoteClick: function(e){
+        this.editField.focus();
+        getSelection().collapseToEnd();
+     },
+    
+    // On the release of key
+     onKeyUp: function(e){
+        this.dirty = true;
+         this.saveSoon();
+         
+     }
+    
 }
